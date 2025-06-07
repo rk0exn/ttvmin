ef56931f8d153eb0958b9e44dd0f4fb9989e2922
rk0exn <214826087+rk0exn@users.noreply.github.com>
2025-06-07 23:59:53 +0900

    Uploaded

---
+++ Helpers/ColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using ttvmin.Models;
+
+namespace ttvmin.Helpers;
+
+public sealed class ColorConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		string colorString = value as string;
+		if (string.IsNullOrWhiteSpace(colorString)) return Brushes.Transparent;
+
+		try
+		{
+			var color = new ColorFormatProvider(colorString).ToColor();
+			return new SolidColorBrush(color);
+		}
+		catch
+		{
+			return Brushes.Transparent;
+		}
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		=> throw new NotImplementedException();
+}
---
+++ Helpers/Extension.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+using ttvmin.Models;
+
+namespace ttvmin.Helpers;
+
+public static class Extension
+{
+	public static Color ToColor(this ColorFormatProvider provider) => provider.Color;
+}
