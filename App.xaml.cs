38ff6bbe4f640af6fefc85db4c062394fde2d2e0
rk0exn <214826087+rk0exn@users.noreply.github.com>
2025-06-08 00:07:07 +0900

    Uploaded

---
+++ App.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace ttvmin;
+
+public sealed partial class App : Application
+{
+	[DllImport("uxtheme.dll", EntryPoint = "#135")]
+	private static extern void AllowDarkModeForApp(int iEnable);
+
+	[DllImport("user32.dll")]
+	private static extern void SetForegroundWindow(nint hwnd);
+
+	public static new App Current => _current;
+	private static App _current;
+
+#if DEBUG
+	private Mutex mx;
+#endif
+
+	protected override void OnStartup(StartupEventArgs e)
+	{
+#if DEBUG
+		mx = new(true, "ttvmin", out var createdNew);
+		if (!createdNew)
+		{
+			var currentId = Process.GetCurrentProcess().Id;
+			foreach (var p in Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName))
+			{
+				if (p.Id != currentId && (nint)p.MainWindowHandle != 0)
+				{
+					SetForegroundWindow(p.MainWindowHandle);
+					Environment.Exit(0);
+					return;
+				}
+			}
+		}
+#endif
+		if (Debugger.IsAttached
+#if DEBUG
+			&& !(File.Exists(@"S:\source\3dview\bin\Release\3dview.exe") && GenerateHash().ToLower() == "5f8e0529e689770b69f4860e8a1eb9ab70013367ee73324ccfcac81ec94fed64c5f02d7f9850415678964cda2eea5c916dbd229976da5d485c5f912f16387786".ToLower())
+#endif
+			)
+		{
+			MessageBox.Show("リリース版はデバッグできません！", "エラー", MessageBoxButton.OK, MessageBoxImage.Hand);
+			Environment.FailFast("DO NOT DEBUG PLEASE!!!");
+			Process.GetCurrentProcess().Kill();
+			return;
+		}
+		base.OnStartup(e);
+	}
+
+#if DEBUG
+	protected override void OnExit(ExitEventArgs e)
+	{
+		if (mx != null)
+		{
+			mx.ReleaseMutex();
+			mx.Dispose();
+		}
+		base.OnExit(e);
+	}
+
+	private string GenerateHash()
+	{
+		try
+		{
+			using var s5 = SHA512.Create();
+			StringBuilder sb = new();
+			foreach (var c in s5.ComputeHash(File.ReadAllBytes(@"S:\source\3dview\bin\Release\3dview.exe")))
+			{
+				sb.Append($"{c:X2}");
+			}
+			return sb.ToString();
+		}
+		catch { return ""; }
+	}
+#endif
+
+	public App()
+	{
+		AllowDarkModeForApp(2);
+		_current = this;
+	}
+
+	public object GetResByStr(string key) => FindResource(key);
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,206 @@
+﻿#pragma warning disable IDE0079
+#pragma warning disable IDE1006
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace ttvmin;
+
+public partial class MainWindow : Window
+{
+	[DllImport("dwmapi.dll")]
+	private static extern void DwmSetWindowAttribute(nint hwnd, int pv, ref int dw, int cb);
+
+	[DllImport("user32.dll")]
+	private static extern int GetWindowLong(nint hwnd, int id);
+
+	[DllImport("user32.dll")]
+	private static extern void SetWindowLong(nint hwnd, int id, int dwnewlong);
+
+	[DllImport("user32.dll")]
+	private static extern nint GetSystemMenu(nint hwnd, bool bRevert);
+
+	[DllImport("user32.dll")]
+	private static extern int TrackPopupMenu(nint hmenu, int uFlags, int x, int y, int nReserved, nint hwnd, ref RECT prcRect);
+
+	[DllImport("user32.dll")]
+	private static extern void SendMessage(nint hwnd, int msg, nint wp, nint lp);
+
+	[DllImport("user32.dll")]
+	private static extern void GetWindowRect(nint hwnd, out RECT rc);
+
+	[StructLayout(LayoutKind.Sequential)]
+	private struct RECT
+	{
+		public int left;
+		public int top;
+		public int right;
+		public int bottom;
+	}
+
+	public static MainWindow Current => _current;
+	private static MainWindow _current;
+
+	public static bool IsLight => _isLight;
+
+	private static bool _isLight = true;
+
+	private bool IsChanging = false;
+
+	private bool _isFullScreen = false, _switching = false;
+	private WindowState _previousWindowState, _prevState;
+	private WindowStyle _previousWindowStyle;
+
+	private nint hWnd => IsLoaded ? new WindowInteropHelper(this).Handle : 0;
+
+	public MainWindow()
+	{
+		_current = this;
+		InitializeComponent();
+#if DEBUG
+		Title += " - Debug";
+#endif
+		if (Debugger.IsAttached) Title += "(Debugger Attached)";
+		KeyDown += (_, e) =>
+		{
+			if (e.Key == Key.F11) ToggleFullscreenMode();
+		};
+		Loaded += (_, _) =>
+		{
+			int r = 4;
+			DwmSetWindowAttribute(hWnd, 38, ref r, sizeof(int));
+			SetWindowLong(hWnd, -16, GetWindowLong(hWnd, -16) & ~0x80000);
+			CloseButton.Click += (_, _) => Close();
+			MaxRsButton.Click += (_, _) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+			MinmzButton.Click += (_, _) =>
+			{
+				if (WindowState != WindowState.Minimized)
+				{
+					_prevState = WindowState;
+				}
+				WindowState = WindowState == WindowState.Minimized ? _prevState : WindowState.Minimized;
+			};
+			SizeChanged += (_, _) =>
+			{
+				if (WindowState == WindowState.Maximized)
+				{
+					grid.Margin = new(7);
+					MaxRsButton.Content = "\uE923";
+					MinmzButton.Content = "\uE921";
+				}
+				else if (WindowState == WindowState.Minimized)
+				{
+					MaxRsButton.Content = "\uE922";
+					MinmzButton.Content = "\uE923";
+				}
+				else
+				{
+					grid.Margin = new(0);
+					MaxRsButton.Content = "\uE922";
+					MinmzButton.Content = "\uE921";
+				}
+			};
+			HwndSource.FromHwnd(hWnd).AddHook(WndProc);
+			SwitchTheme_();
+		};
+	}
+
+	private nint WndProc(nint hwnd, int msg, nint wp, nint lp, ref bool handled)
+	{
+		if (msg == 0x112 && wp == 0xF120 && _isFullScreen)
+		{
+			handled = true;
+		}
+		else if (msg == 0x112 && wp == 0xF100 && !_isFullScreen)
+		{
+			RECT rc = new();
+			GetWindowRect(hWnd, out var wndRect);
+			var r = TrackPopupMenu(GetSystemMenu(hWnd, false), 0x1500, wndRect.left, wndRect.top + 32, 0, hWnd, ref rc);
+			SendMessage(hWnd, 0x112, r, 0);
+			handled = true;
+		}
+		else if (msg == 0x1a)
+		{
+			SwitchTheme_();
+		}
+		return 0;
+	}
+
+	private void SwitchTheme_()
+	{
+		bool r0 = false;
+		try
+		{
+			using var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize");
+			if ((int)key.GetValue("AppsUseLightTheme", 0) == 1)
+			{
+				r0 = true;
+			}
+		}
+		catch { }
+		SwitchTheme(r0);
+	}
+
+	private Style GetStyle(string key) => (Style)App.Current.GetResByStr(key);
+
+	private void SwitchTheme(bool isl)
+	{
+		if (IsChanging || isl == IsLight) return;
+		IsChanging = true;
+		var i = isl ? 0 : 1;
+		DwmSetWindowAttribute(hWnd, 20, ref i, sizeof(int));
+		Resources.Clear();
+		ResourceDictionary rd = [];
+		string y = isl ? "Light" : "Dark";
+		Style[] basedStyles = [GetStyle($"{y}DefSys"), GetStyle($"{y}Text")];
+		foreach (var style in basedStyles)
+		{
+			rd.Add(style.TargetType, new Style(style.TargetType, style));
+		}
+		CloseButton.Style = GetStyle($"{y}SysClose");
+		Resources = rd;
+		_isLight = isl;
+		Views.TrainTimetableView.Current.SwitchTheme(isl);
+		IsChanging = false;
+	}
+
+	private void ToggleFullscreenMode()
+	{
+		if (_switching) return;
+		_switching = true;
+		if (!_isFullScreen)
+		{
+			_previousWindowState = WindowState;
+			_previousWindowStyle = WindowStyle;
+			if (_previousWindowState == WindowState.Maximized)
+			{
+				WindowState = WindowState.Normal;
+			}
+			wc.CaptionHeight = 0;
+			gr0.Visibility = Visibility.Collapsed;
+			row0.Height = new(0, GridUnitType.Pixel);
+			ResizeMode = ResizeMode.NoResize;
+			WindowStyle = WindowStyle.None;
+			WindowState = WindowState.Maximized;
+			_isFullScreen = true;
+		}
+		else
+		{
+			if (_previousWindowState == WindowState.Maximized)
+			{
+				WindowState = WindowState.Normal;
+			}
+			ResizeMode = ResizeMode.CanResize;
+			row0.Height = new(32, GridUnitType.Pixel);
+			gr0.Visibility = Visibility.Visible;
+			wc.CaptionHeight = 32;
+			WindowStyle = _previousWindowStyle;
+			WindowState = _previousWindowState;
+			_isFullScreen = false;
+		}
+		_switching = false;
+	}
+}
