0799b73552c97b312c6e4a41415a8c5dde5be547
rk0exn <214826087+rk0exn@users.noreply.github.com>
2025-06-08 00:02:08 +0900

    Upload AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+[assembly: AssemblyTitle("時刻表ビューア コンパクト版")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Train Timetable Viewer")]
+[assembly: AssemblyCopyright("Copyright (c) 2025 All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("3.3.0.1")]
+[assembly: AssemblyFileVersion("3.3.0.1")]
